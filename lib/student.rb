02f7868035df32486449c6a99096a63a5942d968
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-17 18:21:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,44 +5,20 @@ class Student
 
   def initialize(student_hash)
     student_hash.each {|key, value| self.send(("#{key}="), value)}
-    @@all << self
+    @@all.push(self)
+  end
+  
+   def self.all
+    @@all
   end
 
   def self.create_from_collection(students_array)
     students_array.each {|student_hash| Student.new(student_hash)}
   end
-
+  
   def add_student_attributes(attributes_hash)
     attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
-
-  def self.all
-    @@all
-  end
-
-  
-=begin
-
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
-
-  @@all = []
-
-  def initialize(student_hash)
-    
-  end
-
-  def self.create_from_collection(students_array)
-    
-  end
-
-  def add_student_attributes(attributes_hash)
-    
-  end
-
-  def self.all
-    
-  end
-=end  
 
 end
 
