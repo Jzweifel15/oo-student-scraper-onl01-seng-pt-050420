13740c058257d3320ce62e90da123b5759829bee
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-17 18:13:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
       student_name = student.css("h4.student-name").text
       student_location = student.css("p.student-location").text
       student_profile_url = "https://learn-co-cirriculum.github.io/student-scraper-test-page/students/#{student.css("a").attribute("href").value}"
-      student_index_array.push({name => student_name, location => student_location, profile_url => student_profile_url})
+      student_index_array.push({name: student_name, location: student_location, profile_url: student_profile_url})
     end 
     student_index_array 
   end 
