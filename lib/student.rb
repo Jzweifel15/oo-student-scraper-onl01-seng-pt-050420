4b44f5eb5a5e05f0444c712ae8d8e9fd4d8f1024
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-17 18:20:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,28 @@
 class Student
 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+  @@all = []
+
+  def initialize(student_hash)
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
+
+  def self.create_from_collection(students_array)
+    students_array.each {|student_hash| Student.new(student_hash)}
+  end
+
+  def add_student_attributes(attributes_hash)
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
+  end
+
+  def self.all
+    @@all
+  end
+
+  
+=begin
+
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
 
   @@all = []
@@ -19,5 +42,7 @@ class Student
   def self.all
     
   end
+=end  
+
 end
 
